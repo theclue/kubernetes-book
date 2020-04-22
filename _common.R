30d01e4f058eaccb9076959233d092472b5affeb
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2020-04-23 00:52:40 +0200

    Initial skaffholding

---
+++ _common.R
@@ -0,0 +1,51 @@
+library(methods)
+set.seed(1014)
+
+# options(lifecycle_warnings_as_errors = TRUE)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+options(
+  rlang_trace_top_env = rlang::current_env(),
+  rlang__backtrace_on_error = "none"
+)
+
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut")
+)
+
+if (knitr::is_latex_output()) {
+  knitr::opts_chunk$set(width = 69)
+  knitr::opts_chunk$set(fig.pos = "H")
+  options(width = 69)
+  options(crayon.enabled = FALSE)
+  options(cli.unicode = TRUE)
+  options(knitr.graphics.auto_pdf = TRUE)
+}
+
+knitr::knit_hooks$set(
+  small_mar = function(before, options, envir) {
+    if (before) {
+      par(mar = c(4.1, 4.1, 0.5, 0.5))
+    }
+  }
+)
+
+error_wrap <- function(x, width = getOption("width")) {
+  lines <- strsplit(x, "\n", fixed = TRUE)[[1]]
+  paste(strwrap(lines, width = width), collapse = "\n")
+}
